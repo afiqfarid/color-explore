0aacd7a5ff8526eec3930dd756bd5be5be05f545
unknown <afiqfaridm@gmail.com>
2022-06-20 20:32:22 +0800

    third commit

--- assets/main.js
+++ assets/main.js
@@ -6,9 +6,18 @@ for (let index = 0; index < 30; index++) {
     colorContainerEl.classList.add("color-box");
 
     containerEl.appendChild(colorContainerEl);
+
+    /* const colorLabel = document.createElement("span");
+
+    colorLabel.classList.add("color-label");
+
+    colorContainerEl.appendChild(colorLabel); */
+
 }
 
 const colorContainerElAll = document.querySelectorAll(".color-box");
+
+const colorLabelAll = document.querySelectorAll(".color-box span");
 
 // console.log(colorContainerElAll);
 
@@ -18,14 +27,22 @@ function generateColors(){
     colorContainerElAll.forEach((colorContainerEl)=>{
         const newColorCode = randomColor();
         const newColorCode2 = randomColor();
-        colorContainerEl.style.background = "#" + newColorCode;
-        // colorContainerEl.style.background = "linear-gradient(to right, #" + newColorCode + ", #" + newColorCode2 + ")";
-        colorContainerEl.innerText = "#" + newColorCode;
+        colorContainerEl.style.background = "linear-gradient(45deg, #" + newColorCode + ", #" + newColorCode2 + ")";
+
+        const colorLabel = document.createElement("span");
+
+        colorLabel.classList.add("color-label");
+
+        colorContainerEl.appendChild(colorLabel);
+
+        colorLabel.innerText = "#" + newColorCode + ", #" + newColorCode2;
+        // colorLabel.innerText = "Copy 👆";
+        // colorLabel.innerHTML = "<i class='bx bxs-copy' ></i>";
     })
 }
 
 function randomColor() {
-    const chars = "0123456789abcdef";
+    const chars = "0123456789ABCDEF";
     const colorCodeLength = 6;
     let colorCode = ""
 
